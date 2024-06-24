cb68dba9f2e17f2f0c1dc9b4915a9e04fca4191e
lixinxiu <lixinxiu@xiaomi.com>
2024-06-24 14:51:47 +0800

    jac

--- symbol_ws/Rk2SymU1.m
+++ symbol_ws/Rk2SymU1.m
@@ -3,8 +3,8 @@ clear, clc;
 syms x y theta delta v a u1 u2 L k dt
 
 % 定义状态变量和状态向量
-state = [x; y; theta; delta; v; a];
-control = [u1; u2];
+state = [x; y; theta; delta];
+control = [u1];
 
 % 定义状态方程
 f = [
@@ -12,13 +12,11 @@ f = [
     v * sin(theta);
     v * tan(delta) / (L * (1 + k * v^2));
     u1;
-    a;
-    u2
 ];
 
 % Runge-Kutta 2 方法离散化
 k1 = f;
-k2 = subs(f, [x, y, theta, delta, v, a], [x + k1(1) * dt * 0.5, y + k1(2) * dt  * 0.5, theta + k1(3) * dt * 0.5, delta + k1(4) * dt * 0.5, v + k1(5) * dt * 0.5, a + k1(6) * dt * 0.5]);
+k2 = subs(f, [x, y, theta, delta], [x + k1(1) * dt * 0.5, y + k1(2) * dt  * 0.5, theta + k1(3) * dt * 0.5, delta + k1(4) * dt * 0.5]);
 
 % 新的状态变量
 state_new = state + k2 * dt;
